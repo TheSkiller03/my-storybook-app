3f2472a8ccf8af167d66b7d0d66465dd839757d3
TheSkiller03 <theskiller243@gmail.com>
2025-04-14 10:43:32 +0200

    aggiunta la stories del Button

---
+++ src/Button.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+    component: Button,
+    tags: ['autodocs'],
+    title: 'Components/Button',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+    args: {
+        label: 'Primary Button',
+        onClick: () => alert('Button clicked!'),
+    },
+};
+
